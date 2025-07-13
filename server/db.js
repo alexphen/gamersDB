9bda48253b9f1d790a60251e1bee9ac07b5b498d
alexphen <alexphen@vt.edu>
2025-07-13 10:05:25 -0400

    fixed segfault

--- server/db.js
+++ server/db.js
@@ -1,7 +1,5 @@
 const oracledb = require('oracledb');
-// require('dotenv').config();
-oracledb.initOracleClient({ libDir: 'E:\\Oracle\\instantclient_23_8' });
-// process.env.TNS_ADMIN = 'E:\\Oracle\\instantclient_23_8\\network\\admin';
+require('dotenv').config();
 
 const poolPromise = oracledb.createPool({
   user: process.env.ORACLE_USER,
