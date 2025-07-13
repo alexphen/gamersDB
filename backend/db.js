ac0bfe4271587c3582c1797cf7ae8a65e2d7045a
alexphen <alexphen@vt.edu>
2025-07-13 09:53:04 -0400

    connection string

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const oracledb = require('oracledb');
+console.log("oracledb")
 require('dotenv').config();
 
 oracledb.initOracleClient();
@@ -6,7 +7,7 @@ oracledb.initOracleClient();
 const poolPromise = oracledb.createPool({
   user: process.env.ORACLE_USER,
   password: process.env.ORACLE_PASSWORD,
-  connectString: process.env.ORACLE_CONNECT_STRING,
+  connectString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=g1e4482f6c79339_gamersdb_low.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))",
   poolMin: 2,
   poolMax: 10,
   poolIncrement: 1
