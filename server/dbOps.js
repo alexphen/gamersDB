b780c58974faaef5e670ae992f229cf33640b572
alexphen <alexphen@vt.edu>
2025-07-13 22:36:05 -0400

    updated add/remove gamer

--- server/dbOps.js
+++ server/dbOps.js
@@ -135,13 +135,14 @@ class DbOps {
 		const conn = await oracledb.getConnection();
 		try {
 			const result = await conn.execute(
-				`SELECT game, players, CAST(gamers AS gamer_names_type) AS gamer_list FROM games;`
+				`SELECT game, players, CAST(gamers AS gamer_names_type) AS gamer_list FROM games`
 			);
 			
 			// console.log('Raw result:', result.rows);
 			
 			const items = result.rows.map(row => {
 				console.log("RAW gamers:", row.GAMER_LIST);
+				console.log("RAW gamers:", typeof(row[3]));
 				return {
 					rowid: row[0],
 					game: row[1],
