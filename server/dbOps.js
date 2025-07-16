cb8ed658d7312d5e51d41507e2d57de1874704d4
alexphen <alexphen@vt.edu>
2025-07-16 14:56:07 -0400

    gamer list update

--- server/dbOps.js
+++ server/dbOps.js
@@ -14,7 +14,7 @@ class DbOps {
         const conn = await oracledb.getConnection();
         try {
             const result = await conn.execute(
-                `SELECT rowid, game, players, gamers, fullPartyOnly, remotePlay FROM GAMES ORDER BY game COLLATE NOCASE`
+                `SELECT rowid, game, players, gamers, fullPartyOnly, remotePlay FROM GAMES ORDER BY lower(game)`
             );
             
             const items = result.rows.map(row => ({
